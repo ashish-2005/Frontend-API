b529df6c4cf2a3aac42789457c4fee4bfb942c40
ashishchaubey <ashish.23b0121172@abes.ac.in>
2026-02-22 10:29:15 +0530

    initial commit

---
+++ backend/usefsmodule.js
@@ -0,0 +1,54 @@
+const fs=require('fs')
+const fs1=require('fs').promises
+
+function dataWrite(){
+    let statusmsg="";
+    try{
+    fs.writeFileSync('studentdata.txt',"Welcome to FS module in NODE ")
+    console.log("Data written successfully!!!")
+    statusmsg="Data written successfully!!!";
+    }catch(e){
+        console.log(e)
+        statusmsg="Error is:"+e;
+    }
+return statusmsg
+}
+
+function dataRead(){
+    let statusmsg=" ";
+
+
+try{
+    const fdata=fs.readFileSync('studentdata.txt',{encoding:'utf-8'})
+     statusmsg=fdata;
+     
+}catch(e){
+    statusmsg=e;
+}
+return statusmsg
+}
+
+function deleteFile(){
+    try{
+         fs.unlinkSync('studentdata.txt')
+         statusmsg="File deleting successfully"
+    }
+    catch(e){
+          statusmsg="Error during deletion:"+e
+    }
+    return statusmsg
+}
+
+async function readFileAsysnc(){
+    let statusmsg=""
+    try{
+         statusmsg= fs1.readFile('student.json',{encoding:'utf-8'})
+    }
+    catch(e){
+      statusmsg=e;
+    }
+    return statusmsg
+}
+
+const obj = {dataWrite,dataRead,deleteFile,readFileAsysnc}
+module.exports=obj;
